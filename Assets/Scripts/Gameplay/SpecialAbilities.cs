385b101bb726b062f320097dc90b0e652e8d4d41
Maksym Shestakov <maksshestakov107@gmail.com>
2023-04-08 21:21:34 +0200

    Added interfaces for special abilities methods

---
+++ Assets/Scripts/Gameplay/SpecialAbilities.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialAbilities : MonoBehaviour
+{
+    protected GameObject Ship;
+
+    protected void SetTurboModeAbility()
+    {
+        print("TurboModeAbility");
+    }
+
+    protected void SetTransparencyAbility()
+    {
+        print("TransparencyAbility");
+    }
+
+    protected void SetShootingAbility()
+    {
+        print("ShootingAbility");
+    }
+
+    protected void SetProtectiveShieldAbility()
+    {
+        print("ProtectiveShieldAbility");
+    }
+
+    protected void SetAutoPilotAbility()
+    {
+        print("AutoPilotAbility");
+    }
+
+    protected void SetRadarAbility()
+    {
+        print("RadarAbility");
+    }
+
+    protected void SetXAbility()
+    {
+        print("XAbility");
+    }
+}
---
+++ Assets/Scripts/Others/SpecialAbilityManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpecialAbilityManager : SpecialAbilities
+{
+    [SerializeField] private Slider Slider;
+    [SerializeField] private Button Button;
+    private string[] ShipNames = { "CargoA(Clone)", "CargoB(Clone)", "Miner(Clone)", 
+        "SpeederA(Clone)", "SpeederB(Clone)", "SpeederC(Clone)", "SpeederD(Clone)" };
+
+
+    private void Start()
+    {
+        DataHolder.specialAbility = 0;
+        StartCoroutine(SpecialAbilityStatus());
+    }
+
+    public void UseSpecialAbility() 
+    {
+        Ship = GameObject.FindGameObjectWithTag("Ship");
+        DataHolder.specialAbility = 0;
+        DataHolder.fuel = 30;
+
+        for(int i = 0; i < ShipNames.Length; i++) 
+        {
+            if(ShipNames[i] == Ship.name) 
+            {
+                switch (i) 
+                {
+                    case 0:
+                        SetTurboModeAbility();
+                        break;
+                    case 1:
+                        SetTransparencyAbility();
+                        break;
+                    case 2:
+                        SetShootingAbility();
+                        break;
+                    case 3:
+                        SetProtectiveShieldAbility();
+                        break;
+                    case 4:
+                        SetAutoPilotAbility();
+                        break;
+                    case 5:
+                        SetRadarAbility();
+                        break;
+                    case 6:
+                        SetXAbility();
+                        break;
+                }
+            }
+        }
+    }
+
+    private IEnumerator SpecialAbilityStatus() 
+    {
+        while (true) 
+        {
+            Slider.value = DataHolder.specialAbility;
+            Button.gameObject.SetActive(DataHolder.specialAbility == 5 ? true : false);
+
+            yield return null;
+        }
+    }
+}
